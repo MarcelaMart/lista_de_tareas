bfbdc1782ff797e3c6c835d79fcc86ac31e611e4
Marcela <marcelamartinezc089@gmail.com>
2023-03-07 11:23:40 -0500

    print main menu

---
+++ main.py
@@ -0,0 +1,19 @@
+def run():
+    print("LISTA DE TAREAS")
+    print("1. Agregar nueva tarea (no disponible)")
+    print("2. Ver la lista de  tarea (no disponible)")
+    print("3. Quitar una tarea (no disponible)")
+    print("4. Editar una tarea (no disponible)")
+    print("5. marcar una tarea como terminada (no disponible)")
+    print
+    option = input("escoja la accion a ejecutar y dar enter: ")
+    print(f"escogio la opcion {option} ")
+
+
+
+
+
+
+
+if __name__=="__main__":
+    run()
